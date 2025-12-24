50dbb3ba8eecc593d699eb2f01b1313fa94ae98c
Xdshka1 <superdimsuper2006@gmail.com>
2025-12-25 01:10:23 +0300

    first commit

---
+++ src/MazeRenderer.java
@@ -0,0 +1,27 @@
+public final class AsciiMazeRenderer {
+    public String render(Maze maze) {
+        int rows = maze.rows();
+        int cols = maze.cols();
+        StringBuilder out = new StringBuilder();
+
+        out.append('+');
+        for (int c = 0; c < cols; c++)
+            out.append(maze.northWall(0, c) ? "---+" : "   +");
+        out.append('\n');
+
+        for (int r = 0; r < rows; r++) {
+            out.append(maze.westWall(r, 0) ? '|' : ' ');
+            for (int c = 0; c < cols; c++) {
+                out.append("   ");
+                out.append(maze.eastWall(r, c) ? '|' : ' ');
+            }
+            out.append('\n');
+            out.append('+');
+            for (int c = 0; c < cols; c++)
+                out.append(maze.southWall(r, c) ? "---+" : "   +");
+            out.append('\n');
+        }
+
+        return out.toString();
+    }
+}
